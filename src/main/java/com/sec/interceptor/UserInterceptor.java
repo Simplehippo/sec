d3d927da6b2b48cafd9ba98c24fd8313eeb9df45
杨育才 <1127424957@qq.com>
2019-04-22 00:57:59 +0800

    添加刷新缓存过期时间和cookie的操作

--- src/main/java/com/sec/interceptor/UserInterceptor.java
+++ src/main/java/com/sec/interceptor/UserInterceptor.java
@@ -1,10 +1,14 @@
 package com.sec.interceptor;
 
 import com.sec.common.Const;
+import com.sec.service.func.RedisService;
 import com.sec.util.ThreadLocalUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +21,9 @@ public class UserInterceptor implements HandlerInterceptor {
 
     public static final Logger log = LoggerFactory.getLogger(UserInterceptor.class);
 
+    @Autowired
+    private RedisService redisService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -25,6 +32,7 @@ public class UserInterceptor implements HandlerInterceptor {
                 if(Const.COOKIE_TOKEN_NAME.equals(cookie.getName())) {
 //                    log.info("<!>--> UserInterceptor preHandle(): cookie_name: {}, val: {}", cookie.getName(), cookie.getValue());
                     ThreadLocalUtil.setThreadLocalToken(cookie.getValue());
+                    flushTokenExpire(cookie.getValue(), response);
                     return true;
                 }
             }
@@ -35,12 +43,32 @@ public class UserInterceptor implements HandlerInterceptor {
         String paramToken = request.getParameter("token");
         if(paramToken != null) {
             ThreadLocalUtil.setThreadLocalToken(paramToken);
+            flushTokenExpire(paramToken, response);
+            return true;
         } else {
 //            log.info("<!>--> UserInterceptor preHandle(): paramToken is null");
         }
 
         return true;
     }
+
+
+    /**
+     * 此时还有请求说明用户还在使用
+     * 刷新token过期时间
+     */
+    private void flushTokenExpire(String token, HttpServletResponse response) {
+        // 刷新token过期时间
+        redisService.expire(RedisService.USER_TOKEN_PREFIX, token);
+
+        // 刷新Cookie
+        Cookie cookie = new Cookie(Const.COOKIE_TOKEN_NAME, token);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(RedisService.DEFAULT_EXPIRE_TIME);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
